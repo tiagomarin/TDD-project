6c39cea117464bd9dfefeee45aae0aa9da5cf41b
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-19 19:54:59 -0300

    setup Rspec & linter | add empty methods & files

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+rspec-3.11.0
+gem 'rubocop', '>= 1.0', '< 2.0'
---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+  def factorial(N)
+
+  end
+  def reverse(word)
+
+  end
+  def fizzbuzz(N)
+    
+  end
+end

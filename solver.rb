7d7b891b10495dc2a1ccd28e1074ca60a9f382c7
Tiago Marin <tiago.lelinski@gmail.com>
2022-10-19 23:51:13 -0300

    add fizzbuzz method that passes the tests

--- solver.rb
+++ solver.rb
@@ -1,13 +1,21 @@
 class Solver
-  def self.factorial(num)
-  
-  end
+  # def self.factorial(num)
+    
+  # end
 
-  def self.reverse(word)
+  # def self.reverse(word)
   
-  end
+  # end
 
   def self.fizzbuzz(num)
-
+    if num % 3 == 0 && num % 5 == 0
+      "fizzbuzz" 
+    elsif num % 5 == 0
+      "buzz"
+    elsif num % 3 == 0
+      "fizz"
+    else
+      num.to_s
+    end
   end
 end
